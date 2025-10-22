a112f7c3ebbc9f25f9f344a9b4a74ce1e3a88f1f
VicHGC <victor.grajeda02@gmail.com>
2025-10-21 21:48:57 -0600

    Agregue la funcionalidad de sesion persistente para el padre

--- src/controllers/userLogin.js
+++ src/controllers/userLogin.js
@@ -2,6 +2,10 @@ const pool = require('../config/db.js'); // importo el pool del archivo db.js qu
 
 const bcrypt = require('bcrypt'); //importo bcrypt que es una libreria para hashear las contrasenias.
 
+const jwt = require('jsonwebtoken'); // importo esto para hacer un json web token y hacer la sesion persisntente
+
+const crypto = require('crypto'); // con esta libreria creare un token qr aleatorio para guardarlo en la tabla de tokens_qr
+
 const crearUsuario = async (req, res) => { // la coloco en async para poder manejar las peticiones de manera asincrona y que el backend o servidor siempre este tomando peticiones.
     const {email, password} = req.body; // del body del req saco los datos de email y password
 
@@ -47,21 +51,50 @@ const loginUsuarioEmail = async (req, res) => {
 
         const user = rows[0];
 
-        const passwordIsValid = await bcrypt.compare(password, user.password_hash);
+        const passwordIsValid = await bcrypt.compare(password, user.password_hash); // comparamos las contrasenias en hash
 
         if (!passwordIsValid) {
             return res.status(401).send("Email o contraseña incorrectos");
         }
 
+        // aqui se crea el JWT para la sesion persistente
+
+        const accessToken = jwt.sign( // esto le dara un acceso temporal al usuario
+            {"userID": user.id}, // payload, estos son los datos que tendra el token
+            process.env.JWT_ACCESS_SECRET,
+            {expiresIn: '15m'} // esto quiere decir que expira en 15 min
+        );
+
+        const refreshToken = jwt.sign(
+            {"userID": user.id},
+            process.env.JWT_REFRESH_SECRET,
+            {expiresIn: '7d'} // expirara en 7 dias.
+        );
+
+        const saveTokenSql = "UPDATE accounts SET refresh_token = ? where id = ?"; // guardamos el token en la base de datos
+
+        await pool.query(saveTokenSql, [refreshToken, user.id]);
+
+        res.cookie('jwt', refreshToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'None',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
         res.status(200).json({
             message: "Login exitoso",
+            accessToken: accessToken,
             userID: user.id
         });
+
+
     } catch (error){
         console.error(error);
         res.status(500).send("Error interno del servidor");
     }
 }
+
 
 module.exports = {
     crearUsuario,
