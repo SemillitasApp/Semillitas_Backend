e66fdebb0dc31c79628cfe43f1c646727aec68ba
VicHGC <victor.grajeda02@gmail.com>
2025-10-24 22:07:32 -0600

    corrijo variables de entorno

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config(); // libreria para leer las variables de entorno del .env
 
 const pool = mysql.createPool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 module.exports = pool;
